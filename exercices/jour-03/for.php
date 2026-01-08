09b399db006082033687872b0add7785947c041d
moussafassi <moussa.fassi@le-campus-numerique.fr>
2026-01-08 16:43:57 +0100

    Jour 3 boucles

---
+++ exercices/jour-03/for.php
@@ -0,0 +1,22 @@
+<pre>
+<?php
+
+for ($i = 0; $i <= 10; $i++) {
+    echo $i;
+}; 
+echo"<br>";
+for ($i = 0; $i <= 20; $i+=2) {
+    echo $i;
+};
+echo "<br>";
+for( $i =10 ;$i >= 0; $i=$i-1 ){
+    echo $i;
+};
+echo "<br>";
+for( $i = 0; $i <= 10; $i++){
+echo $i*7," ";
+}
+?>
+
+
+</pre>
--- exercices/jour-03/foreach-assoc.php
+++ exercices/jour-03/foreach-assoc.php
@@ -13,6 +13,15 @@ $teams=[
     <title>Document</title>
 </head>
 <body>
-    
+    <ul>
+        <?php foreach($teams as $team): ?>
+         <li>
+            <?php foreach($team as $key => $value): ?>
+                 <strong><?= $key." :"?></strong><strong><?= " ".$value." "?></strong><br>
+
+                <?php endforeach;?>
+         </li>  
+         <?php endforeach;?>
+    </ul>
 </body>
 </html>
